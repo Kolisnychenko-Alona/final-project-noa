03ecbf453ec6e0aab85c55a7cbe8ceba1657728e
Kolisnychenko-Alona <tisspac@gmail.com>
2023-04-26 15:45:42 +0300

    added styles and logic to header component

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -2,11 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { DeliveryComponent } from 'src/app/pages/delivery/delivery.component';
 import { AuthDialogComponent } from '../auth-dialog/auth-dialog.component';
+import { BasketDialogComponent } from '../basket-dialog/basket-dialog.component';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
   public deliveryType: string = `Доставка кур'єром`;
@@ -17,6 +18,30 @@ export class HeaderComponent implements OnInit {
   constructor(public dialog: MatDialog) {}
 
   ngOnInit(): void {}
+
+  down(btn: HTMLElement): void {
+    btn.classList.add('down');
+  }
+  up(btn: HTMLElement): void {
+    btn.classList.remove('down');
+  }
+  openCatalog(catalog: HTMLElement): void {
+    catalog.classList.add('openCatalog');
+  }
+  closeCatalog(catalog: HTMLElement): void {
+    catalog.classList.remove('openCatalog');
+  }
+  openProducts(catalog: HTMLElement): void {
+    this.closeCatalog(catalog);
+  }
+  openPhoneDialog(phone: HTMLElement): void {
+    console.log(phone);
+    phone.classList.add('openPhone');
+  }
+  closePhoneDialog(phone: HTMLElement): void {
+    console.log('close phone', phone);
+    phone.classList.remove('openPhone');
+  }
 
   openDeliveryDialog(): void {
     this.dialog.open(DeliveryComponent, {
@@ -34,11 +59,12 @@ export class HeaderComponent implements OnInit {
       height: '200px',
     });
   }
-  
-  down(btn: HTMLElement): void{
-    btn.classList.add('down');
-  }
-  up(btn: HTMLElement): void{
-    btn.classList.remove('down');
+  openBasketDialog(): void {
+    this.dialog.open(BasketDialogComponent, {
+      backdropClass: 'dialog-back',
+      width: '500px',
+      height: '100vh',
+      position: { top: '0', right: '0'}
+    });
   }
 }
