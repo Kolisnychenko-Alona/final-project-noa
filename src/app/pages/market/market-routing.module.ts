ad90a5a8fd5d15a249e9b449974e3df754a64c1d
Kolisnychenko-Alona <tisspac@gmail.com>
2023-05-16 15:10:49 +0300

    fixed bugs

--- src/app/pages/market/market-routing.module.ts
+++ src/app/pages/market/market-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MarketComponent } from './market.component';
 import { ThaiProductInfoResolver } from 'src/app/shared/services/thai-product/thai-product-info.resolver';
-import { MarketProductInfoComponent } from './market-product-info/market-product-info/market-product-info.component';
+import { MarketProductInfoComponent } from './market-product-info/market-product-info.component';
 
 const routes: Routes = [
   {
@@ -12,7 +12,7 @@ const routes: Routes = [
   {
     path: ':id',
     component: MarketProductInfoComponent,
-    resolve: { thaiProductInfo: ThaiProductInfoResolver},
+    resolve: { thaiProductInfo: ThaiProductInfoResolver },
   },
 ];
 
