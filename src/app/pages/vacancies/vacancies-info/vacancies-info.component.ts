e848ee9add913607ccb22404ccaf4fc372f2d672
Kolisnychenko-Alona <tisspac@gmail.com>
2023-05-17 23:05:40 +0300

    added elements and styles

--- src/app/pages/vacancies/vacancies-info/vacancies-info.component.ts
+++ src/app/pages/vacancies/vacancies-info/vacancies-info.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { IVacancyResponse } from 'src/app/shared/interfaces/vacancy/IVacancy';
 
 @Component({
   selector: 'app-vacancies-info',
   templateUrl: './vacancies-info.component.html',
-  styleUrls: ['./vacancies-info.component.scss']
+  styleUrls: ['./vacancies-info.component.scss'],
 })
-export class VacanciesInfoComponent {
+export class VacanciesInfoComponent implements OnInit {
+  public vacancy!: IVacancyResponse;
 
+  constructor(private activatedRout: ActivatedRoute) {}
+  ngOnInit(): void {
+    this.activatedRout.data.subscribe((response) => {
+      this.vacancy = response['vacancyInfo'];
+      // this.separatedText = this.discount.text.split('.');
+      // this.separatedText.pop();
+    });
+  }
 }
