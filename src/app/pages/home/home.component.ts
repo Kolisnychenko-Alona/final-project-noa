5c47bd9e08b08a3af8f9cd0fe8ec84380ac961e6
Kolisnychenko-Alona <tisspac@gmail.com>
2023-05-22 21:17:56 +0300

    modified files

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  public more = false;
 
+  constructor() {}
+  ngOnInit(): void { }
+  
+  toggleMore(moreText: HTMLElement): void{
+    this.more = !this.more;
+    const moreTextWidth = moreText.scrollHeight + 'px';
+    if (this.more) {
+      moreText.style.height = moreTextWidth;
+    } else if(!this.more){
+      moreText.style.height = '0';
+    }
+    console.log(this.more);
+    
+  }
 }
